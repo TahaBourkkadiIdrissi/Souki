46fa14bf507be2f781f3b374943162093ee54de6
TahaBourkkadiIdrissi <t.bourkkadiidrissi@esisa.ac.ma>
2026-04-11 22:02:05 +0100

    Maked sure that the front is setted up

---
+++ front-end/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenue sur Souki</h1>
+      <p>Projet React + Next.js configuré et prêt à être utilisé.</p>
+    </main>
+  );
+}
