fb0905383c9b5e22acb5ee65ac4b07d3b76bf4fc
Salah-Sghiri <sghirisalaheddine@gmail.com>
2026-04-16 21:42:35 +0100

    configuration de config.py

--- back-end/config.py
+++ back-end/config.py
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
 import os
 from dotenv import load_dotenv
-=======
->>>>>>> main
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
-<<<<<<< HEAD
+
 load_dotenv()
-# Database Config
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "00%2B9ALAS")
-DB_NAME = os.getenv("DB_NAME", "db_souki")
-DB_PORT = os.getenv("DB_PORT", "5432")
-=======
-# Load environment variables from .env
-load_dotenv()
->>>>>>> main
 
 # Fetch variables
 USER     = os.getenv("user")
@@ -30,10 +18,8 @@ DBNAME   = os.getenv("dbname")
 # Construct the SQLAlchemy connection string
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
-<<<<<<< HEAD
-engine = create_engine(URL, pool_size=10, pool_pre_ping=True) 
+engine = create_engine(DATABASE_URL, pool_size=10, pool_pre_ping=True) 
 LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-=======
 # Créer le moteur avec un pool de connexions
 # pool_size=10 → max 10 connexions simultanées
 # max_overflow=5 → 5 connexions supplémentaires si le pool est plein
@@ -49,7 +35,6 @@ engine = create_engine(
 LocalSession = sessionmaker(bind=engine)
 
 # Base déclarative — tous vos modèles vont hériter de cette classe
->>>>>>> main
 Base = declarative_base()
 
 
